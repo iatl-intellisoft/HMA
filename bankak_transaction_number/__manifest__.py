496e60f2caf92d5c4e464beb0c8761b76f60aefc
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-02-17 13:20:48 +0200

    Add files via upload

---
+++ bankak_transaction_number/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Bankak Transaction Number',
+    'version': '1.0',
+    'summary': 'Add Bankak Transaction Number with duplicate validation',
+    'depends': ['account'],
+    'data': [
+        'views/account_payment_view.xml',
+    ]
+}
---
+++ bankak_transaction_number/models/account_payment.py
@@ -0,0 +1,24 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    bankak_transaction_number = fields.Char(
+        string="Bankak Transaction Number",
+        copy=False
+    )
+
+    @api.constrains('bankak_transaction_number')
+    def _check_duplicate_bankak_number(self):
+        for rec in self:
+            if rec.bankak_transaction_number:
+                existing = self.search([
+                    ('bankak_transaction_number', '=', rec.bankak_transaction_number),
+                    ('id', '!=', rec.id)
+                ], limit=1)
+                
+                if existing:
+                    raise ValidationError(
+                        "لقد تم تحويل مبلغ بنفس رقم العملية!"
+                    )

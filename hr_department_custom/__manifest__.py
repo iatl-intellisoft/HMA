4650d91b75dd678d6a82e26a5217361463e382f4
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-02-25 10:00:01 +0200

    Add files via upload

---
+++ hr_department_custom/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HR Department Custom",
+
+    'summary': """
+        """,
+
+    'description': """
+    """,
+
+    'author': "IATL-intellisoft International",
+    'website': "http://www.iatl-intellisoft.com",
+    'version': '18.0',
+    'license': 'LGPL-3',
+    'category': 'Human Resources',
+    'depends': ['hr', 'analytic'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/hr_views.xml',
+        'views/res_user_view.xml',
+    ],
+}
---
+++ hr_department_custom/models/res_users.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class Users(models.Model):
+    _inherit = "res.users"
+
+    def _default_groups(self):
+        default_user = self.env.ref('base.group_user', raise_if_not_found=False)
+        return default_user
+
+    groups_id = fields.Many2many('res.groups', 'res_groups_users_rel', 'uid', 'gid', string='Groups',
+                                 default=_default_groups)
+    department_id = fields.Many2one('hr.department', string='Department', related=False, company_dependent=True)
+    allowed_department_ids = fields.Many2many('hr.department', 'rdepartment_users_rel', 'uid', 'department_id',
+                                              string='Allowed Departments',)

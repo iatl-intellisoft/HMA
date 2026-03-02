f3eeb4e7afaec6bc92e05744ac69a23dbb9f903a
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-03-02 15:02:58 +0200

    Update __manifest__.py

--- exchange_cost_update/__manifest__.py
+++ exchange_cost_update/__manifest__.py
@@ -4,15 +4,14 @@
 
     'summary': "exchange cost update",
 
-    'description': """exchange cost update
-    """,
+    'description': """  exchange cost update  """,
 
     'author': "IATL-Intelloft", 
+    
     'depends': ['product', 
                 'account'],
     'data': [   
         'security/ir.model.access.csv',
-        'report/car_attachment.xml',
         'views/product_view.xml'
     ], 
 }

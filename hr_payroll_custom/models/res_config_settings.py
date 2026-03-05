d4a007d990ed7046f76df16ecf0f56f9c9594fde
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-03-05 13:36:59 +0200

    Update res_config_settings.py

--- hr_payroll_custom/models/res_config_settings.py
+++ hr_payroll_custom/models/res_config_settings.py
@@ -6,8 +6,8 @@ class ResConfigSettings(models.TransientModel):
 
     _inherit = 'res.config.settings'
    
-    first_employee_id_cu = fields.Many2one('hr.employee', string='first employee sign consider',related="company_id.first_employee_id", ondelete='set null', readonly=False)
-    second_employee_id_cu = fields.Many2one('hr.employee', string='second employee sign consider',related="company_id.second_employee_id", ondelete='set null', readonly=False)
+    first_employee_id_cu = fields.Many2one('hr.employee', string='first employee sign consider',related="company_id.first_employee_id_cu", ondelete='set null', readonly=False)
+    second_employee_id_cu = fields.Many2one('hr.employee', string='second employee sign consider',related="company_id.second_employee_id_cu", ondelete='set null', readonly=False)
 
 
 class Company(models.Model):

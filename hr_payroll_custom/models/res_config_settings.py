dd87ebd3aa5625bed6f036f9c5f6a36c03e2e24d
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-03-05 13:40:22 +0200

    Update res_config_settings.py

--- hr_payroll_custom/models/res_config_settings.py
+++ hr_payroll_custom/models/res_config_settings.py
@@ -6,12 +6,12 @@ class ResConfigSettings(models.TransientModel):
 
     _inherit = 'res.config.settings'
    
-    first_employee_id_cu = fields.Many2one('hr.employee', string='first employee sign consider',related="company_id.first_employee_id_cu", ondelete='set null', readonly=False)
-    second_employee_id_cu = fields.Many2one('hr.employee', string='second employee sign consider',related="company_id.second_employee_id_cu", ondelete='set null', readonly=False)
+    first_employee_id = fields.Many2one('hr.employee', string='first employee sign consider',related="company_id.first_employee_id", ondelete='set null', readonly=False)
+    second_employee_id = fields.Many2one('hr.employee', string='second employee sign consider',related="company_id.second_employee_id", ondelete='set null', readonly=False)
 
 
 class Company(models.Model):
     _inherit = 'res.company'
 
-    first_employee_id_cu = fields.Many2one('hr.employee', string='First employee sign consider', ondelete='set null' )
-    second_employee_id_cu = fields.Many2one('hr.employee', string='Second employee sign consider', ondelete='set null' )
+    first_employee_id = fields.Many2one('hr.employee', string='First employee sign consider', ondelete='set null' )
+    second_employee_id = fields.Many2one('hr.employee', string='Second employee sign consider', ondelete='set null' )

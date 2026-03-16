49021592324eaf46420797c7d60b8e40674f6ca3
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-03-16 13:48:54 +0200

    Add files via upload

---
+++ payment_request/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import account_move
+from . import account_payment
+from . import payment_request
+from . import res_config_settings
+from . import payment_clearance
+from . import truck_odometer

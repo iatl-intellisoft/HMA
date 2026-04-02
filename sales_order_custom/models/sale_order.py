d8cb7197cc5bce8e5059e4f18403235586dbc444
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-04-02 12:18:22 +0200

    Add truck and driver fields to SaleOrder and StockPicking

--- sales_order_custom/models/sale_order.py
+++ sales_order_custom/models/sale_order.py
@@ -8,14 +8,20 @@ class ShippingDestination(models.Model):
     code = fields.Integer(string="Code")
     name = fields.Char(string="City Name")
 
-   
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-
+    truck_id = fields.Many2one('fleet.vehicle', string="Truck")
+    driver_id = fields.Many2one(
+            'res.partner',
+            string="Driver",
+            related="truck_id.driver_id",
+            store=True,
+            readonly=True
+        )
     shipping_office_name = fields.Char(string="اسم مكتب الشحن", store=True)
     shipping_office_number = fields.Char(string="رقم مكتب الشحن", store=True)
     shipping_destination = fields.Many2one('shipping.destination', string="مكان ارسال البضاعة", store=True)
- 
+
     def action_confirm(self):
         res = super().action_confirm()
 
@@ -26,40 +32,27 @@ class SaleOrder(models.Model):
 
         return res
 
-
-
-    def action_register_payment(self):
-        self.ensure_one()
-
-         
-        invoices = self.invoice_ids.filtered(
-            lambda inv: inv.state == 'posted' and inv.payment_state != 'paid'
-        )
-
-        if not invoices:
-            raise UserError("لا توجد فواتير متاحة للسداد (قد تكون مدفوعة أو قيد التسوية)")
-
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Register Payment',
-            'res_model': 'account.payment.register',
-            'view_mode': 'form',
-            'target': 'new',
-            'context': {
-                'active_model': 'account.move',
-                'active_ids': invoices.ids,
-            },
-        }
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-
+    truck_id = fields.Many2one('fleet.vehicle' ,related='sale_id.truck_id', string="Truck" , readonly="1")
+    driver_id = fields.Many2one(
+            'res.partner',
+            string="Driver",
+            related="truck_id.driver_id",
+            store=True,
+            readonly=True
+        )
     shipping_office_name = fields.Char(related='sale_id.shipping_office_name', store=True)
     shipping_office_number = fields.Char(related='sale_id.shipping_office_number', store=True)
     shipping_destination = fields.Many2one('shipping.destination', string="مكان ارسال البضاعة", store=True)
-    
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
     shipping_receipt = fields.Binary(string="ايصال الشحن")
     shipping_receipt_name = fields.Char(string="اسم الملف")
+
+    @api.model
+    def create(self, vals):
+        if vals.get('sale_id'):
+            sale = self.env['sale.order'].browse(vals['sale_id'])
+            if sale.driver_id:
+                vals['driver_id'] = sale.driver_id.id
+
+        return super().create(vals) 

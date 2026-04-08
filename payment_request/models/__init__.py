bfe4c118fd4a368c58ebdd9816a5a90e3538fb07
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-04-08 23:04:57 +0200

    Update __init__.py

--- payment_request/models/__init__.py
+++ payment_request/models/__init__.py
@@ -4,5 +4,4 @@ from . import account_move
 from . import account_payment
 from . import payment_request
 from . import res_config_settings
-from . import payment_clearance
-from . import truck_odometer
+from . import payment_clearance 

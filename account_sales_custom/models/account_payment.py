4ff34c503d8e9452e8fa85fccb28ce02f0c13a7c
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-04-16 10:25:43 +0200

    Update account_payment.py

--- account_sales_custom/models/account_payment.py
+++ account_sales_custom/models/account_payment.py
@@ -16,38 +16,14 @@ class AccountPayment(models.Model):
                 raise ValidationError("يجب إختيار عميل أو حساب")
             if rec.partner_id and rec.manual_account_id:
                 raise ValidationError(" إختيار عميل وعميل في نفس الوقت غير مسموح به")
-    def _prepare_move_line_default_vals(self, write_off_line_vals=None):
-        res = super()._prepare_move_line_default_vals(write_off_line_vals)
+    def _prepare_move_line_default_vals(self, write_off_line_vals=None, force_balance=None):
+        res = super()._prepare_move_line_default_vals(
+            write_off_line_vals=write_off_line_vals,
+            force_balance=force_balance
+        )
 
-        for line in res: 
-            if not self.partner_id and self.manual_account_id:
-                if line.get('manual_account_id') == self.destination_manual_account_id.id:
-                    line['manual_account_id'] = self.manual_account_id.id
-
-        return res      
-# class SaleOrder(models.Model):
-#     _inherit = 'sale.order'
-    
-#     def action_register_payment(self):
-#         self.ensure_one()
-
-         
-#         invoices = self.invoice_ids.filtered(
-#             lambda inv: inv.state == 'posted' and inv.payment_state != 'paid'
-#         )
-
-#         if not invoices:
-#             raise UserError("لا توجد فواتير متاحة للسداد (قد تكون مدفوعة أو قيد التسوية)")
-
-#         return {
-#             'type': 'ir.actions.act_window',
-#             'name': 'Register Payment',
-#             'res_model': 'account.payment.register',
-#             'view_mode': 'form',
-#             'target': 'new',
-#             'context': {
-#                 'active_model': 'account.move',
-#                 'active_ids': invoices.ids,
-#             },
-#         }
-
+        if not self.partner_id and self.manual_account_id:
+            for line in res:
+                if line.get('account_id') != self.journal_id.default_account_id.id:
+                    line['account_id'] = self.manual_account_id.id
+                    break  

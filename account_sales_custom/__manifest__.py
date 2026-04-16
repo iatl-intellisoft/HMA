615efd616183930d6479adaf69663071104196c8
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-04-16 10:17:55 +0200

    Delete account_sales_custom directory

--- account_sales_custom/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'account & sales custom',
-    'version': '1.0',
-    'summary': 'Add Bankak Transaction Number with duplicate validation',
-    'depends': ['account','sale'],
-    'data': [
-        'views/account_payment_view.xml',
-    ]
-}

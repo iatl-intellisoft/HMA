66f7758306900d97a46c18973cf3bd97a38a7711
iatl-intellisoft <72150721+iatl-intellisoft@users.noreply.github.com>
2026-04-26 15:31:05 +0300

    Delete product_foreign_currency_costing directory

--- product_foreign_currency_costing/models/res_company.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    foreign_cost_currency_id = fields.Many2one('res.currency', string='Foreign Cost Currency')

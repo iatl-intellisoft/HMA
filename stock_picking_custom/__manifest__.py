5bdadd7733d271a84f21dd4c9fe7e3f629fb436c
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-04-28 08:50:53 +0200

    Add files via upload

---
+++ stock_picking_custom/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Stock Picking Custom', 
+    'depends': ['stock'],
+    'description': """ Stock Picking Custom """,
+    'author': "IATL-intellisoft International",
+    'website': "http://www.iatl-intellisoft.com", 
+    'license': 'LGPL-3',
+    'data': [ 
+   			 'views/stock_picking_views.xml',
+    ], 
+}

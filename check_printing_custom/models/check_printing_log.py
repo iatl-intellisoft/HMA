e225de4cb6dff2bb1153dbda1fa433eb09e4a71a
iatl-intellisoft <72150721+iatl-intellisoft@users.noreply.github.com>
2026-04-29 10:43:08 +0300

    Refactor CheckPrintingReason model methods and fields

--- check_printing_custom/models/check_printing_log.py
+++ check_printing_custom/models/check_printing_log.py
@@ -5,25 +5,43 @@ from odoo import api, fields, models
 class CheckPrintingReason(models.Model):
     _name = "check.printing.log"
     _description = 'Check Printing Log'
-    payment_id = fields.Many2one('account.payment', string=u'payment', ondelete='set null', readonly=True)
-    reason_id = fields.Many2one('check.printing.reason', string='Reason', ondelete='set null', readonly=True)
+    _order = 'print_date desc, id desc'
+
+    payment_id = fields.Many2one(
+        'account.payment',
+        string='Payment',
+        ondelete='set null',
+        readonly=True,
+    )
+    reason_id = fields.Many2one(
+        'check.printing.reason',
+        string='Reason',
+        ondelete='set null',
+        readonly=True,
+    )
     check_no = fields.Integer(string='Check No', readonly=True)
-    beneficiary = fields.Char(string="Beneficiary", readonly=True)
+    beneficiary = fields.Char(string='Beneficiary', readonly=True)
+    print_date = fields.Datetime(
+        string='Print Date',
+        readonly=True,
+        default=fields.Datetime.now,
+        help="Date and time when the check was printed.",
+    )
 
-    @api.model
-    def create(self, vals):
-        if vals['payment_id']:
-            payment = vals['payment_id']
-            beneficiary = self.env['account.payment'].search([('id', '=', payment)])
-            vals['beneficiary'] = beneficiary.beneficiary
-        return super(CheckPrintingReason, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('payment_id'):
+                payment = self.env['account.payment'].browse(vals['payment_id'])
+                if payment.exists():
+                    vals['beneficiary'] = payment.beneficiary
+            if not vals.get('print_date'):
+                vals['print_date'] = fields.Datetime.now()
+        return super().create(vals_list)
 
-    # @api.multi
-    def name_get(self):
-        res = []
+    @api.depends('payment_id', 'check_no')
+    def _compute_display_name(self):
         for record in self:
-            payment_id = str(record.payment_id.name)
-            check_no = str(record.check_no)
-            res.append((record.id, payment_id + ' / ' + check_no))
-
-        return res
+            payment_name = record.payment_id.name or ''
+            check_no = str(record.check_no or '')
+            record.display_name = '%s / %s' % (payment_name, check_no)

8537088b8bde1273781d07a7ed953569a0331439
iatl-intellisoft <72150721+iatl-intellisoft@users.noreply.github.com>
2026-05-05 13:53:11 +0300

    Delete ii_simple_check_management_modified directory

--- ii_simple_check_management_modified/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import payment
-from . import check
-from . import money_to_text_en
-from . import money_to_text_ar
-from . import res_config_settings
--- ii_simple_check_management_modified/report/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import Report_check_template

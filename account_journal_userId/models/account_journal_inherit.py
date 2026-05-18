ecea30c17579fd77c099b6457d9487db89930bdf
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-05-18 09:05:01 +0200

    Delete account_journal_userId directory

--- account_journal_userId/models/account_journal_inherit.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-class AccountJournal(models.Model):
-    _inherit = 'account.journal'
-
-    user_id = fields.Many2one(
-        'res.users',
-        string='إسم المستخدم'
-    )

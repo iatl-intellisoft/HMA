7d94254736a067bb2b76e3e414de84e5a28a82c8
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-01 12:14:10 +0200

    Update movement_operation_report.py

--- payment_request/wizards/movement_operation_report.py
+++ payment_request/wizards/movement_operation_report.py
@@ -73,7 +73,7 @@ class MovementOperationReport(models.TransientModel):
         sheet.col(8).width = sheet.col(9).width = sheet.col(10).width = sheet.col(11).width = sheet.col(12).width = sheet.col(13).width = sheet.col(14).width = sheet.col(15).width =sheet.col(16).width =sheet.col(17).width =sheet.col(18).width=sheet.col(19).width = 5000
 
         # Write header
-        sheet.write_merge(0, 0, 0, 19,  f'({self.start_date})/({self.end_date}) كشف متابعة حركو وتشغيل الدفارات للفترة ', main_heading)
+        sheet.write_merge(0, 0, 0, 17,  f'({self.end_date})/({self.start_date}) كشف متابعة و تشغيل الدفارات للفترة ', main_heading)
         sheet.write(1, 0, "رقم الشاحنة", heading)
         sheet.write(1, 1, "اسم السائق", heading)
         sheet.write(1, 2, "تاريخ التعبئة", heading)
@@ -83,20 +83,17 @@ class MovementOperationReport(models.TransientModel):
         sheet.write(1, 6, "معدل استهلاك الوقود(لتر/كم)", heading)
         sheet.write(1, 7, "مجموع التكاليف(لتر*سعر الوقود)", heading)
         sheet.write(1, 8, "عداد المسافات", heading)
-        sheet.write(1, 9, "غيار زيت الدوري", heading)
-        sheet.write(1, 10, "الصيانة الدورية", heading)
-        sheet.write(1, 11, "مغسلة", heading)
-        sheet.write(1, 12, "بنشر", heading)
-        sheet.write(1, 13, "مخالفات مرورية", heading)
-        sheet.write(1, 14, "تأمين", heading)
-        sheet.write(1, 15, "قيمة التأمين", heading)
-        sheet.write(1, 16, "تفتيش شهري", heading)
-        sheet.write(1, 17, "صيانات", heading)
-        sheet.write(1, 18, "الاجمالي", heading)
-        sheet.write(1, 19, "ملاحظات", heading)
+        sheet.write(1, 9, "الصيانة", heading)
+        sheet.write(1, 10, "مغسلة", heading)
+        sheet.write(1, 11, "بنشر", heading)
+        sheet.write(1, 12, "مخالفات حكومية", heading)
+        sheet.write(1, 13, "تأمين", heading)
+        sheet.write(1, 14, "تفتيش شهري", heading)
+        sheet.write(1, 15, "أخرى", heading) 
+        sheet.write(1, 16, "ملاحظات", heading)
         
         row = 2   
-        total_oil_change = total_maintenance = total_car_wash = total_car_tire_repair = total_violations = total_insurance = 0
+        total_oil_change = total_maintenance = total_car_wash = total_car_tire_repair = total_violations = total_insurance = total_monthly_inspection = total_other = 0
         for rec in operating_costs:
             if rec.state == "paid":    
                 sheet.row(row).height = 400
@@ -111,42 +108,46 @@ class MovementOperationReport(models.TransientModel):
                 # sheet.write(row, 6, "استلام", content_format1)
                 # sheet.write(row, 7, "" , content_format)
                 # sheet.write(row, 8, ref, content_format) 
-                if rec.custody_type == 'oil_change':
-                    sheet.write(row, 9, rec.total_amount, content_format) 
-                    total_oil_change += rec.total_amount
                 if rec.custody_type == 'maintenance':
-                    sheet.write(row, 10, rec.total_amount, content_format) 
+                    sheet.write(row, 9, rec.total_amount, content_format) 
                     total_maintenance += rec.total_amount
                 if rec.custody_type == 'car_wash':
-                    sheet.write(row, 11, rec.total_amount, content_format) 
+                    sheet.write(row, 10, rec.total_amount, content_format) 
                     total_car_wash += rec.total_amount
                 if rec.custody_type == 'car_tire_repair':
-                    sheet.write(row, 12, rec.total_amount, content_format) 
+                    sheet.write(row, 11, rec.total_amount, content_format) 
                     total_car_tire_repair += rec.total_amount
                 if rec.custody_type == 'violations':
-                    sheet.write(row, 13, rec.total_amount, content_format)  
-                    total_violations += rec.total_amount
+                    sheet.write(row, 12, rec.total_amount, content_format) 
+                    total_violations += rec.total_amount 
                 if rec.custody_type == 'insurance':
-                    sheet.write(row, 14, rec.total_amount, content_format)  
+                    sheet.write(row, 13, rec.total_amount, content_format)  
                     total_insurance += rec.total_amount
-                row += 1 
- 
+                if rec.custody_type == 'monthly_inspection':
+                    sheet.write(row, 14, rec.total_amount, content_format)  
+                    total_monthly_inspection += rec.total_amount
+                if rec.custody_type == 'other':
+                    sheet.write(row, 15, rec.total_amount, content_format)  
+                    total_other += rec.total_amount
+                sheet.write(row, 16, rec.niration, content_format)  
+                row += 1  
         sheet.row(row).height = 400
         sheet.write(row, 0, "المجموع", currency_format)
         sheet.write(row, 1, "-", currency_format)
         sheet.write(row, 2, "-", currency_format)
-        # sheet.write(row, 3,  , currency_format)
-        # sheet.write(row, 4,  , currency_format)
+        sheet.write(row, 3,  , currency_format)
+        sheet.write(row, 4,  , currency_format)
         sheet.write(row, 5, "-", currency_format)
         sheet.write(row, 6, row-2, currency_format)
         sheet.write(row, 7, row-2, currency_format)
         sheet.write(row, 8, "-", currency_format) 
-        sheet.write(row, 9, total_oil_change , currency_format)
-        sheet.write(row, 10, total_maintenance , currency_format)
-        sheet.write(row, 11, total_car_wash , currency_format)
-        sheet.write(row, 12, total_car_tire_repair , currency_format) 
-        sheet.write(row, 13, total_violations , currency_format) 
-        sheet.write(row, 14, total_insurance , currency_format) 
+        sheet.write(row, 9, total_maintenance , currency_format)
+        sheet.write(row, 10, total_car_wash , currency_format)
+        sheet.write(row, 11, total_car_tire_repair , currency_format)
+        sheet.write(row, 12, total_violations , currency_format) 
+        sheet.write(row, 13, total_insurance , currency_format) 
+        sheet.write(row, 14, total_monthly_inspection , currency_format) 
+        sheet.write(row, 15,  , currency_format) 
     
         stream = BytesIO()
         workbook.save(stream)

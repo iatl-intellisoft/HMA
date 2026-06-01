76c14c0ad3cb95c4db53f160c6c6592b98f180a4
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-01 16:48:02 +0200

    Update movement_operation_report.py

--- payment_request/wizards/movement_operation_report.py
+++ payment_request/wizards/movement_operation_report.py
@@ -21,22 +21,14 @@ class MovementOperationReport(models.TransientModel):
 
     def action_generate_movement_operation_excel(self):
         """Generate and return Excel file for movement operation"""
-        records = self.env['custody.clearance'].search([
-            ('date', '>=', self.start_date),
-            ('date', '<=', self.end_date),
-        ])
+       
         operating_costs = self.env['payment.request'].search([ 
             ('date', '>=', self.start_date),
             ('date', '<=', self.end_date),
-            # ('state', '==', 'paid'),
-            # ('is_need_clearance', '==', False),
+            ('state', '==', 'paid'),
+            ('is_need_clearance', '==', False),
         ])
-        truck_odometers = self.env['fleet.vehicle.odometer'].search([
-            ('date', '>=', self.start_date),
-            ('date', '<=', self.end_date),
-            # ('state', '==', 'paid'),
-            # ('is_need_clearance', '==', False),
-        ])
+        
 
         workbook = xlwt.Workbook(encoding='utf-8')
         sheet = workbook.add_sheet('movement operation', cell_overwrite_ok=True)
@@ -70,56 +62,81 @@ class MovementOperationReport(models.TransientModel):
         sheet.col(8).width = sheet.col(9).width = sheet.col(10).width = sheet.col(11).width = sheet.col(12).width = sheet.col(13).width = sheet.col(14).width = sheet.col(15).width =sheet.col(16).width =sheet.col(17).width =sheet.col(18).width=sheet.col(19).width = 5000
 
         # Write header
-        sheet.write_merge(0, 0, 0, 16,  f'({self.end_date})/({self.start_date}) كشف متابعة و تشغيل الدفارات للفترة ', main_heading)
+        sheet.write_merge(0, 0, 0, 14,  f'({self.end_date})/({self.start_date}) كشف متابعة و تشغيل الدفارات للفترة ', main_heading)
         sheet.write(1, 0, "رقم الشاحنة", heading)
         sheet.write(1, 1, "اسم السائق", heading)
         sheet.write(1, 2, "تاريخ التعبئة", heading)
         sheet.write(1, 3, "كمية الوقود(لتر)", heading)
         sheet.write(1, 4, "سعر الوقود(لتر)", heading)
-        sheet.write(1, 5, "المسافة المقطوعة(كم)", heading)
-        sheet.write(1, 6, "معدل استهلاك الوقود(لتر/كم)", heading)
-        sheet.write(1, 7, "مجموع التكاليف(لتر*سعر الوقود)", heading)
-        sheet.write(1, 8, "عداد المسافات", heading)
-        sheet.write(1, 9, "الصيانة", heading)
-        sheet.write(1, 10, "مغسلة", heading)
-        sheet.write(1, 11, "بنشر", heading)
-        sheet.write(1, 12, "مخالفات حكومية", heading)
-        sheet.write(1, 13, "تأمين", heading)
-        sheet.write(1, 14, "تفتيش شهري", heading)
-        sheet.write(1, 15, "أخرى", heading) 
-        sheet.write(1, 16, "ملاحظات", heading)
+        sheet.write(1, 5, "عداد المسافات", heading)
+        # sheet.write(1, 6, "معدل استهلاك الوقود(لتر/كم)", heading)
+        sheet.write(1, 6, "مجموع التكاليف(لتر*سعر الوقود)", heading)
+        sheet.write(1, 7, "الصيانة", heading)
+        sheet.write(1, 8, "مغسلة", heading)
+        sheet.write(1, 9, "بنشر", heading)
+        sheet.write(1, 10, "مخالفات حكومية", heading)
+        sheet.write(1, 11, "تأمين", heading)
+        sheet.write(1, 12, "تفتيش شهري", heading)
+        sheet.write(1, 13, "أخرى", heading) 
+        sheet.write(1, 14, "ملاحظات", heading)
         
         row = 2   
         total_oil_change = total_maintenance = total_car_wash = total_car_tire_repair = total_violations = total_insurance = total_monthly_inspection = total_other = 0
-        for rec in operating_costs:
-            if rec.state == "paid":    
-                sheet.row(row).height = 400
-                sheet.write(row, 0, rec.vehicle_id.license_plate, content_format) 
-                sheet.write(row, 1, rec.vehicle_id.driver_id.name, content_format) 
-                sheet.write(row, 2, rec.date.strftime('%d/%m/%Y'), content_format) 
-                if rec.custody_type == 'maintenance':
-                    sheet.write(row, 9, rec.total_amount, content_format) 
-                    total_maintenance += rec.total_amount
-                if rec.custody_type == 'car_wash':
-                    sheet.write(row, 10, rec.total_amount, content_format) 
-                    total_car_wash += rec.total_amount
-                if rec.custody_type == 'car_tire_repair':
-                    sheet.write(row, 11, rec.total_amount, content_format) 
-                    total_car_tire_repair += rec.total_amount
-                if rec.custody_type == 'violations':
-                    sheet.write(row, 12, rec.total_amount, content_format) 
-                    total_violations += rec.total_amount 
-                if rec.custody_type == 'insurance':
-                    sheet.write(row, 13, rec.total_amount, content_format)  
-                    total_insurance += rec.total_amount
-                if rec.custody_type == 'monthly_inspection':
-                    sheet.write(row, 14, rec.total_amount, content_format)  
-                    total_monthly_inspection += rec.total_amount
-                if rec.custody_type == 'other':
-                    sheet.write(row, 15, rec.total_amount, content_format)  
-                    total_other += rec.total_amount
-                sheet.write(row, 16, rec.niration, content_format)  
-                row += 1  
+        for rec in operating_costs:  
+            fuel_quantity = 0
+            fuel_price = 0
+            fuel_amount = 0
+            date = rec.date
+            vehicle = rec.vehicle_id 
+            vehicle_odometers = 0
+            records = self.env['custody.clearance'].search([ 
+                ('date', '==', date),
+                ('vehicle_id', '==', vehicle),
+                ('state', '==', 'done'),
+            ]) 
+            if records:
+                for rec in  records:
+                    fuel_quantity += rec.custody_line_ids.quantity
+                    fuel_price += rec.custody_line_ids.price_unit
+                    fuel_amount += rec.custody_line_ids.amount
+            truck_odometers = self.env['fleet.vehicle.odometer'].search([ 
+                ('date', '==', date), 
+                ('vehicle_id', '==', vehicle),
+            ])
+            if truck_odometers :
+                for rec in  truck_odometers:
+                    vehicle_odometers = rec.value 
+            sheet.row(row).height = 400
+            sheet.write(row, 0, rec.vehicle_id.license_plate, content_format) 
+            sheet.write(row, 1, rec.vehicle_id.driver_id.name, content_format) 
+            sheet.write(row, 2, rec.date.strftime('%d/%m/%Y'), content_format) 
+            sheet.write(row, 3, fuel_quantity, content_format) 
+            sheet.write(row, 4, fuel_price, content_format) 
+            sheet.write(row, 5, vehicle_odometers, content_format) 
+            sheet.write(row, 6, fuel_amount, content_format) 
+            if rec.custody_type == 'maintenance':
+                sheet.write(row, 7, rec.total_amount, content_format) 
+                total_maintenance += rec.total_amount
+            if rec.custody_type == 'car_wash':
+                sheet.write(row, 8, rec.total_amount, content_format) 
+                total_car_wash += rec.total_amount
+            if rec.custody_type == 'car_tire_repair':
+                sheet.write(row, 9, rec.total_amount, content_format) 
+                total_car_tire_repair += rec.total_amount
+            if rec.custody_type == 'violations':
+                sheet.write(row, 10, rec.total_amount, content_format) 
+                total_violations += rec.total_amount 
+            if rec.custody_type == 'insurance':
+                sheet.write(row, 11, rec.total_amount, content_format)  
+                total_insurance += rec.total_amount
+            if rec.custody_type == 'monthly_inspection':
+                sheet.write(row, 12, rec.total_amount, content_format)  
+                total_monthly_inspection += rec.total_amount
+            if rec.custody_type == 'other':
+                sheet.write(row, 13, rec.total_amount, content_format)  
+                total_other += rec.total_amount
+            sheet.write(row, 14, rec.niration, content_format)  
+            row += 1  
                 
         sheet.row(row).height = 400
         sheet.write(row, 0, "المجموع", currency_format)
@@ -128,17 +145,15 @@ class MovementOperationReport(models.TransientModel):
         sheet.write(row, 3, "" , currency_format)
         sheet.write(row, 4, "", currency_format)
         sheet.write(row, 5, "-", currency_format)
-        sheet.write(row, 6, row-2, currency_format)
-        sheet.write(row, 7, row-2, currency_format)
-        sheet.write(row, 8, "-", currency_format) 
-        sheet.write(row, 9, total_maintenance , currency_format)
-        sheet.write(row, 10, total_car_wash , currency_format)
-        sheet.write(row, 11, total_car_tire_repair , currency_format)
-        sheet.write(row, 12, total_violations , currency_format) 
-        sheet.write(row, 13, total_insurance , currency_format) 
-        sheet.write(row, 14, total_monthly_inspection , currency_format) 
-        sheet.write(row, 15, "", currency_format) 
-        sheet.write(row, 16, "", currency_format) 
+        sheet.write(row, 6, row-2, currency_format) 
+        sheet.write(row, 7, total_maintenance , currency_format)
+        sheet.write(row, 8, total_car_wash , currency_format)
+        sheet.write(row, 9, total_car_tire_repair , currency_format)
+        sheet.write(row, 10, total_violations , currency_format) 
+        sheet.write(row, 11, total_insurance , currency_format) 
+        sheet.write(row, 12, total_monthly_inspection , currency_format) 
+        sheet.write(row, 13, total_other, currency_format) 
+        sheet.write(row, 14, "", currency_format) 
     
         stream = BytesIO()
         workbook.save(stream)

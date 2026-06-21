d0cd733fc5b64eefd815ccb2e209671581ab0977
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-21 11:49:16 +0200

    Update sale_order.py

--- sales_order_custom/models/sale_order.py
+++ sales_order_custom/models/sale_order.py
@@ -46,14 +46,14 @@ class AccountPaymentRegister(models.TransientModel):
         return payments
 class SaleOrder(models.Model):
     _inherit = 'sale.order'  
-    truck_id = fields.Many2one('fleet.vehicle', string="Truck")
-    driver_id = fields.Many2one(
-            'res.partner',
-            string="Driver",
-            related="truck_id.driver_id",
-            store=True,
-            readonly=True
-        )
+    # truck_id = fields.Many2one('fleet.vehicle', string="Truck")
+    # driver_id = fields.Many2one(
+    #         'res.partner',
+    #         string="Driver",
+    #         related="truck_id.driver_id",
+    #         store=True,
+    #         readonly=True
+    #     )
     shipping_office_name = fields.Char(string="اسم مكتب الشحن", store=True)
     shipping_office_number = fields.Char(string="رقم مكتب الشحن", store=True)
     shipping_destination = fields.Many2one('shipping.destination', string="مكان ارسال البضاعة", store=True)
@@ -69,8 +69,6 @@ class SaleOrder(models.Model):
         return res
     def action_register_payment(self):
         self.ensure_one()
-
-         
         invoices = self.invoice_ids.filtered(
             lambda inv: inv.state == 'posted' and inv.payment_state != 'paid'
         )
@@ -96,11 +94,10 @@ class SaleOrder(models.Model):
   
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-    truck_id = fields.Many2one('fleet.vehicle' ,related='sale_id.truck_id', string="Truck" , readonly="1")
+    truck_id = fields.Many2one('fleet.vehicle' , string="Truck" , readonly="1")
     driver_id = fields.Many2one(
             'res.partner',
             string="Driver",
-            related="truck_id.driver_id",
             store=True,
             readonly=True
         )
@@ -110,11 +107,3 @@ class StockPicking(models.Model):
     shipping_receipt = fields.Binary(string="ايصال الشحن")
     shipping_receipt_name = fields.Char(string="اسم الملف")
 
-    # @api.model
-    # def create(self, vals):
-    #     if vals.get('sale_id'):
-    #         sale = self.env['sale.order'].browse(vals['sale_id'])
-    #         if sale.driver_id:
-    #             vals['driver_id'] = sale.driver_id.id 
-
-    #     return super().create(vals) 

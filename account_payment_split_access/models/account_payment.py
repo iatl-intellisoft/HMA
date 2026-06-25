8ec04792213f8b07dc401419dbe2e06d28e57ab9
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-25 10:19:35 +0200

    Update account_payment.py

--- account_payment_split_access/models/account_payment.py
+++ account_payment_split_access/models/account_payment.py
@@ -7,6 +7,10 @@ class AccountPayment(models.Model):
     is_split_payment = fields.Boolean(
     compute="_compute_is_split_payment"
     )
+    customer_cash_payment = fields.Boolean(compute="_compute_split_flags")
+    customer_bank_payment = fields.Boolean(compute="_compute_split_flags")
+    vendor_cash_payment   = fields.Boolean(compute="_compute_split_flags")
+    vendor_bank_payment   = fields.Boolean(compute="_compute_split_flags")
 
     def _compute_is_split_payment(self):
         flag = any([
@@ -24,6 +28,24 @@ class AccountPayment(models.Model):
         ('vendor_cash', 'Vendor Cash'),
         ('vendor_bank', 'Vendor Bank'),
     ])
+    def _compute_split_flags(self):
+        ctx = self.env.context
+        ccp = bool(ctx.get("customer_cash_payment"))
+        cbp = bool(ctx.get("customer_bank_payment"))
+        vcp = bool(ctx.get("vendor_cash_payment"))
+        vbp = bool(ctx.get("vendor_bank_payment"))
+        if ccp or vcp:
+            jtype = "cash"
+        elif cbp or vbp:
+            jtype = "bank"
+        else:
+            jtype = False
+        for rec in self:
+            rec.customer_cash_payment = ccp
+            rec.customer_bank_payment = cbp
+            rec.vendor_cash_payment   = vcp
+            rec.vendor_bank_payment   = vbp
+            rec.split_journal_type    = jtype
 
     @api.model
     def default_get(self, fields_list):

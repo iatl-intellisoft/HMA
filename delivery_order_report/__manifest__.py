2b2e3806fddbbd5012574f02f2d440c70757a51a
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-26 23:32:04 +0200

    Add files via upload

---
+++ delivery_order_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Delivery Order Report',
+    'version': '18.0.1.0.0', 
+    'depends': [
+        'stock',
+        'sale',
+    ],
+    'data': [
+        'data/delivery_order_report.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

faedab05a6f1a0f89cc6ee2220326d8e02868cb6
iatl-intellisoft <72150721+iatl-intellisoft@users.noreply.github.com>
2026-06-28 10:38:50 +0300

    Add files via upload

---
+++ sale_product_stock_qty/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Product Stock Quantity',
+    'version': '18.0.1.0.0',
+    'summary': 'Display product on-hand quantity on sale order lines',
+    'category': 'Sales/Sales',
+    'depends': ['sale_stock'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}

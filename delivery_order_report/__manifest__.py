039ca12faeec2449ca2e88da3f13fee516688ba7
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-28 11:20:21 +0200

    Update __manifest__.py

--- delivery_order_report/__manifest__.py
+++ delivery_order_report/__manifest__.py
@@ -2,11 +2,14 @@
     'name': 'Delivery Order Report',
     'version': '18.0.1.0.0', 
     'depends': [
+        'base',
+        'contacts',
         'stock',
         'sale',
     ],
     'data': [
         'data/delivery_order_report.xml',
+        'views/view_partner.xml'
     ],
     'installable': True,
     'application': False,

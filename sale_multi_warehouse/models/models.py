c09fce4978ddd6d694176065fbe51ce3d3ae0a8d
iatl-intellisoft <72150721+iatl-intellisoft@users.noreply.github.com>
2026-06-29 11:52:00 +0300

    Enhance StockWarehouse with display name computation
    Add display name computation for stock warehouses based on product quantity.

--- sale_multi_warehouse/models/models.py
+++ sale_multi_warehouse/models/models.py
@@ -15,3 +15,16 @@ class SaleOrderLine(models.Model):
         res.update({"warehouse_id": self.warehouse_id})
 
         return res
+
+class StockWarehouse(models.Model):
+    _inherit = 'stock.warehouse'
+
+    @api.depends('name', 'code')
+    def _compute_display_name(self):
+        product_id = self.env.context.get('sale_line_product_id')
+        if not product_id:
+            return super()._compute_display_name()
+        product = self.env['product.product'].browse(product_id)
+        for wh in self:
+            qty = product.with_context(location=wh.lot_stock_id.id).qty_available
+            wh.display_name = '%s - %g qty' % (wh.name, qty)

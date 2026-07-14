4f1d78c4e50ce54a2daa20df79e3da25fe5ad644
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-07-14 20:02:37 +0200

    Delete sales_reports directory

--- sales_reports/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import sale_report_wizard
-from . import sale_report_line
-from . import sale_ops_report_wizard
-from . import sale_ops_report_line
-from . import sale_payment_report_wizard
-from . import sale_payment_report_line

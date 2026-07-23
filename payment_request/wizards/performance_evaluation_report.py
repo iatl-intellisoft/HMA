ada73c5a5b5300a014a0c5314f3a0a54ba165e71
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-07-23 14:12:09 +0200

    Update performance_evaluation_report.py

--- payment_request/wizards/performance_evaluation_report.py
+++ payment_request/wizards/performance_evaluation_report.py
@@ -85,16 +85,14 @@ class PerformanceReport(models.TransientModel):
         picking_not_done = 0        
  
         result = defaultdict(lambda: {
-            'total': {'count': 0, 'amount': 0},
-            'trucks': defaultdict(lambda: {'count': 0, 'amount': 0}),
-            'trucks': defaultdict(lambda: {'delivered_cartons': 0})
+            'total': {'count': 0, 'amount': 0, 'delivered_cartons': 0},
+            'trucks': defaultdict(lambda: {'count': 0, 'amount': 0, 'delivered_cartons': 0})
             
         })
  
         all_result_not_done = defaultdict(lambda: {
             'total': {'count': 0},
-            'trucks': defaultdict(lambda: {'count': 0}),
-            'trucks': defaultdict(lambda: {'delivered_cartons': 0})
+            'trucks': defaultdict(lambda: {'count': 0, 'delivered_cartons': 0})
         })
 
         result_total = 0

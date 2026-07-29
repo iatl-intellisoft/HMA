95c29db974ed235ae05fc3bf8d7506d7dabca6c2
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-07-29 14:56:08 +0200

    Update clearnce_report.py

--- payment_request/wizards/clearnce_report.py
+++ payment_request/wizards/clearnce_report.py
@@ -92,45 +92,44 @@ class ClearnceReport(models.TransientModel):
         custody_amount = 0
         expenses_amount = 0
         for payment_request in payment_requests:
-            if payment_request.request_id not in requests:
-                if requests==[]:
-                    ref="رصيد افتتاحي"
-                else:
-                    ref="تجديد العهدة"
-                requests.append(payment_request.id)
+            if requests==[]:
+                ref="رصيد افتتاحي"
+            else:
+                ref="تجديد العهدة"
+            requests.append(payment_request.name)
+            sheet.row(row).height = 400
+            sheet.write(row, 0, payment_request.date.strftime('%d/%m/%Y'), content_format)
+            sheet.write(row, 1, payment_request.request_id.name, content_format)
+            sheet.write(row, 2, payment_request.employee_id.name, content_format)
+            sheet.write(row, 3, payment_request.custody_amount, content_format)
+            sheet.write(row, 4, "-", content_format)
+            sheet.write(row, 5, payment_request.request_id.base_amount, content_format)
+            sheet.write(row, 6, "استلام", content_format1)
+            sheet.write(row, 7, "" , content_format)
+            sheet.write(row, 8, ref, content_format)
+            custody_amount +=payment_request.custody_amount
+            row += 1
+            clearances = self.env['custody.clearance'].search([
+                ('date', '>=', self.start_date),
+                ('date', '<=', self.end_date),
+                ('state', '=', 'done'),
+                ('request_id', '=', payment_request),
+            ])
+            for clearance in clearances:           
                 sheet.row(row).height = 400
-                sheet.write(row, 0, payment_request.date.strftime('%d/%m/%Y'), content_format)
-                sheet.write(row, 1, payment_request.request_id.name, content_format)
-                sheet.write(row, 2, payment_request.employee_id.name, content_format)
-                sheet.write(row, 3, payment_request.custody_amount, content_format)
-                sheet.write(row, 4, "-", content_format)
-                sheet.write(row, 5, payment_request.request_id.base_amount, content_format)
-                sheet.write(row, 6, "استلام", content_format1)
-                sheet.write(row, 7, "" , content_format)
-                sheet.write(row, 8, ref, content_format)
-                custody_amount +=payment_request.custody_amount
+                sheet.write(row, 0, clearance.date.strftime('%d/%m/%Y'), content_format)
+                sheet.write(row, 1, clearance.request_id.name, content_format)
+                sheet.write(row, 2, clearance.employee_id.name, content_format)
+                sheet.write(row, 3, "-", content_format)
+                sheet.write(row, 4, clearance.total_amount, content_format)
+                sheet.write(row, 5, clearance.request_remaining_amount, content_format)
+                sheet.write(row, 6, "صرف", content_format2)
+                sheet.write(row, 7,  clearance.custody_line_ids.clearance_type, content_format)
+                sheet.write(row, 8, clearance.custody_line_ids.desc, content_format)
+                expenses_amount += clearance.total_amount
                 row += 1
-                clearances = self.env['custody.clearance'].search([
-                    ('date', '>=', self.start_date),
-                    ('date', '<=', self.end_date),
-                    ('state', '=', 'done'),
-                    ('request_id', '=', payment_request),
-                ])
-                for clearance in clearances:           
-                    sheet.row(row).height = 400
-                    sheet.write(row, 0, clearance.date.strftime('%d/%m/%Y'), content_format)
-                    sheet.write(row, 1, clearance.request_id.name, content_format)
-                    sheet.write(row, 2, clearance.employee_id.name, content_format)
-                    sheet.write(row, 3, "-", content_format)
-                    sheet.write(row, 4, clearance.total_amount, content_format)
-                    sheet.write(row, 5, clearance.request_remaining_amount, content_format)
-                    sheet.write(row, 6, "صرف", content_format2)
-                    sheet.write(row, 7,  clearance.custody_line_ids.clearance_type, content_format)
-                    sheet.write(row, 8, clearance.custody_line_ids.desc, content_format)
-                    expenses_amount += clearance.total_amount
-                    row += 1
-                    new_row= row
-                    rema_amount = custody_amount - expenses_amount
+                new_row= row
+                rema_amount = custody_amount - expenses_amount
  
         sheet.row(new_row).height = 400
         sheet.write(new_row, 0, "المجموع", heading)

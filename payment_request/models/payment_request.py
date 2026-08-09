7f884c565e0abffbfe3d94c371bd2270669c7eba
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-08-09 08:52:45 +0200

    Update payment_request.py

--- payment_request/models/payment_request.py
+++ payment_request/models/payment_request.py
@@ -69,7 +69,7 @@ class PaymentRequest(models.Model):
     remaining_amount = fields.Float(
     string="Remaining", 
     store=True,
-    # compute="_onchange_employee_id"
+    compute="_onchange_employee_id"
     )
 
 
@@ -156,7 +156,7 @@ class PaymentRequest(models.Model):
     def _onchange_employee_id(self):
         self.department_id = self.employee_id.department_id.id
         self.partner_id = self.employee_id.user_id.partner_id.id
-        prev_custody = self.env['payment.request'].search([('employee_id','=',self.employee_id.id),('state','=','paid'),('is_need_clearance','=',True)],limit=1)
+        prev_custody = self.env['payment.request'].search([('employee_id','=',self.employee_id.id),('partner_id','=',self.partner_id.id),('state','=','paid'),('is_need_clearance','=',True)],limit=1)
         if prev_custody:
             self.remaining_amount = prev_custody.remaining_amount
 
